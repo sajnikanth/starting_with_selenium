0fa7389d93c3eb6f83f0f24146702e38ca0bdde1
Sajnikanth Suriyanarayanan <sajnikanth@gmail.com>
2015-11-29 20:32:27 +0100

    make a proper test

--- test_albelli.py
+++ test_albelli.py
@@ -1,5 +1,15 @@
-from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://albelli.nl')
-assert browser.find_element_by_id('hero-shot').is_displayed()
-browser.quit()
+import selenium.webdriver
+import unittest
+
+class AlbelliTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = selenium.webdriver.Firefox()
+        self.url = 'http://albelli.nl'
+
+    def test_home_page(self):
+        self.driver.get(self.url)
+        home_page_banner = self.driver.find_element_by_id('hero-shot')
+        assert home_page_banner.is_displayed()
+
+    def tearDown(self):
+        self.driver.quit()
