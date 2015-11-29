c48c47256dc2edcc43563b87ce06387d00fcd80c
Sajnikanth Suriyanarayanan <sajnikanth@gmail.com>
2015-11-29 20:36:48 +0100

    running with holmium

--- test_albelli.py
+++ test_albelli.py
@@ -3,13 +3,8 @@ import unittest
 
 class AlbelliTest(unittest.TestCase):
     def setUp(self):
-        self.driver = selenium.webdriver.Firefox()
-        self.url = 'http://albelli.nl'
+        self.driver.get(self.driver._holmium_config['environment'])
 
     def test_home_page(self):
-        self.driver.get(self.url)
         home_page_banner = self.driver.find_element_by_id('hero-shot')
         assert home_page_banner.is_displayed()
-
-    def tearDown(self):
-        self.driver.quit()
