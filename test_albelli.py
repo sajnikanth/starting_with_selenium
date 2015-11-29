65bc93c3214c5525963b193e92f73b0707404372
Sajnikanth Suriyanarayanan <sajnikanth@gmail.com>
2015-11-29 20:37:52 +0100

    holmium page object

--- test_albelli.py
+++ test_albelli.py
@@ -1,10 +1,13 @@
 import selenium.webdriver
 import unittest
+from holmium.core import Page, Element, Locators
+
+class AlbelliHomePage(Page):
+    home_page_banner = Element(Locators.ID, 'hero-shot', timeout=5)
 
 class AlbelliTest(unittest.TestCase):
     def setUp(self):
-        self.driver.get(self.driver._holmium_config['environment'])
+        self.page = AlbelliHomePage(self.driver, self.driver._holmium_config['environment'])
 
     def test_home_page(self):
-        home_page_banner = self.driver.find_element_by_id('hero-shot')
-        assert home_page_banner.is_displayed()
+        assert self.page.home_page_banner.is_displayed()
